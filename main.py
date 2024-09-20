e455f6d0473bae7a19209893d40a439e07548d21
leox00 <146089334+leox00@users.noreply.github.com>
2024-09-20 15:22:37 +0200

    Fixed create_note with path

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import argparse
 import os
 
-def create_note(note_name):
-    with open(note_name, 'w') as f:
+def create_note(note_name, folder):
+    folder = os.path.join("files", folder)
+    if not os.path.exists(folder) and folder != "files":
+        os.makedirs(folder)
+    file_path = os.path.join(folder, note_name + ".txt")
+    with open(file_path, 'w') as f:
         f.write('')
-    print(f"Note '{note_name}' created.")
-
-def edit_note(note_name):
-    pass
+    print(f"Note '{note_name}' created at '{file_path}'.")
 
 def main():
     parser = argparse.ArgumentParser(description='This is BNotes CLI a notes app with encryption feature and much more!')
@@ -16,17 +17,12 @@ def main():
     # Subparser for creating a note
     command_create = subparsers.add_parser('create', help='Create a plain note')
     command_create.add_argument('name', type=str, help='Name of the note to create')
-
-    # Subparser for editing a note
-    command_edit = subparsers.add_parser('edit', help='Edit a note')
-    command_edit.add_argument('name', type=str, help='Name of the note to edit')
+    command_create.add_argument('--path','-p' ,type=str, default='files', help='Directory folder (without file) to create the note in')
 
     args = parser.parse_args()
 
     if args.command == "create":
-        create_note(args.name)
-    elif args.command == "edit":
-        edit_note(args.name)
+        create_note(args.name, args.path)
     else:
         parser.print_help()
 
